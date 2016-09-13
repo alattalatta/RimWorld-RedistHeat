b88c79498b36eca752e491fb94722a3e4808376d
Morgloz <alejandromvlm@gmail.com>
2016-09-13 16:51:36 +0200

    Added interval configuration for wall checking

---
+++ RedistHeat/CompProperties/CompWallObjectProperties.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace RedistHeat
+{
+    class CompWallObjectProperties : CompProperties
+    {
+        public int checkTickInterval = 120;
+        public CompWallObjectProperties()
+        {
+            this.compClass = typeof(CompWallObject);
+        }
+    }
+}
