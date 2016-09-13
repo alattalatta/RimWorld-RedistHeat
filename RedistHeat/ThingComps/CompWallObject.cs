7221e9070654a0d2902cc1270be94775866aa943
Morgloz <alejandromvlm@gmail.com>
2016-09-13 16:52:33 +0200

    Added wall checking

---
+++ RedistHeat/ThingComps/CompWallObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RedistHeat
+{
+    class CompWallObject : ThingComp
+    {
+
+        public CompWallObjectProperties Props
+        {
+            get
+            {
+                return (CompWallObjectProperties)this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            //#if DEBUG
+            //            Log.Message("Ticking...");
+            //#endif
+            base.CompTick();
+            if (!parent.IsHashIntervalTick(Props.checkTickInterval))
+            {
+                return;
+            }
+            Validate();
+        }
+
+        public override void CompTickRare()
+        {
+            base.CompTickRare();
+            Validate();
+        }
+
+        public void DestroyParent()
+        {
+            parent.Destroy(DestroyMode.Deconstruct);
+        }
+
+
+        public void Validate()
+        {
+//#if DEBUG
+//            Log.Message("Checking...");
+//#endif
+            IntVec3 c = parent.Position;
+            Building wall = c.GetEdifice();
+            if ((wall == null) || ((wall.def.graphicData.linkFlags & LinkFlags.Wall) == 0))
+            {
+                DestroyParent();
+                return;
+            }
+        }
+    }
+}
